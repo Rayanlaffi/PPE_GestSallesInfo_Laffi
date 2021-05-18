26e85b0d25100eb6eb3e12b240d7afb1d3b0c4d1
Rayanlaffi <Rayan.laffi@icof.fr>
2021-05-18 21:17:18 +0200

    create poste

--- Modele/posteManager.php
+++ Modele/posteManager.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once("Modele/Manager.php");
-require_once("modele/poste.php");
+require_once("$racine/Modele/Manager.php");
+require_once("$racine/modele/poste.php");
 
 class posteManager extends Manager
 {
@@ -36,6 +36,25 @@ class posteManager extends Manager
             $poste[$donnees['nPoste']] = new poste($donnees['nPoste'], $donnees['nomPoste'],$donnees['indIP'],$donnees['ad'],$donnees['typePoste'],$donnees['idSalle'],$donnees['nbLog']);
         }
         return $poste;
+        
+    }
+
+    public function createPoste($nomPoste,$indip,$ad,$typePoste,$nbLog) 
+    {
+       $requeteMaxID = $this->getPDO()->query('SELECT MAX(CAST(SUBSTRING(`nPoste`, 2) AS UNSIGNED)) as "maxID" from poste'); // la requete 
+       $maxID = $requeteMaxID->fetch(PDO::FETCH_ASSOC);// ici je recup le tableau qui nous donne la valeur
+       $new_id = $maxID['maxID'] + 1; //ici je rajoute 1 a l'id
+       $nPoste = "p" . $new_id; // ici je remet le 'p' devant
+       $idSalle = null;
+
+       if(empty($indip)){$indip = null;}
+       if(empty($ad)){$ad = null;}
+       if(empty($typePoste)){$typePoste = null;}
+       if(empty($nbLog)){$nbLog = null;}
+
+       $sql = "INSERT INTO poste (nPoste, nomPoste, indIP, ad, typePoste, idSalle, nbLog) VALUES (?,?,?,?,?,?,?)";
+       $stmt= $this->getPDO()->prepare($sql);
+       $stmt->execute([$nPoste, $nomPoste, $indip, $ad, $typePoste, $idSalle, $nbLog]);
         
     }
     
